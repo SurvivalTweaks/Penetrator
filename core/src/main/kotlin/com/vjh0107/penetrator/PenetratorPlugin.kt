1269e9f1f8bfa9894e4e72f8e583778f0e2bf44b
Junhyung <vjh0107@naver.com>
2022-09-05 19:54:27 +0900

    added config loader

--- core/src/main/kotlin/com/vjh0107/penetrator/PenetratorPlugin.kt
+++ core/src/main/kotlin/com/vjh0107/penetrator/PenetratorPlugin.kt
@@ -2,4 +2,8 @@ package com.vjh0107.penetrator
 
 import com.vjh0107.barcode.framework.AbstractBarcodePlugin
 
-class PenetratorPlugin : AbstractBarcodePlugin()
+class PenetratorPlugin : AbstractBarcodePlugin() {
+    override fun enable() {
+        saveDefaultConfig()
+    }
+}
